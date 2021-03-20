8038b04122b4b247c91509e4931eab4829974c81
jwi2018 <60511413+jwi2018@users.noreply.github.com>
2021-03-20 22:19:18 +0900

    GameManagerStage
    GameManagerStage

---
+++ Assets/Scenes/Scripts/Shelter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Shelter : MonoBehaviour
+{
+    public GameObject Upgrade;
+
+    private void Start()
+    {
+        Vector3 pos = new Vector3(34f, -7f, 0f);
+        gameObject.transform.Translate(pos);
+    }
+    //5~-1
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = gameObject.transform.position;
+        Vector3 targetScreenPos = Camera.main.WorldToScreenPoint(pos);
+
+        if (targetScreenPos.x < Screen.width || targetScreenPos.x > 0 || targetScreenPos.y < Screen.height || targetScreenPos.y > 0)
+        {
+            gameObject.transform.Translate(-8f * Time.deltaTime, 0f, 0f);
+        }
+
+        if (targetScreenPos.x + 788 < 0)
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
