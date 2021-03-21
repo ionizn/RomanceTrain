ddbe2dc819a6c385fbf01eb421cd30712f85de05
DESKTOP-BPJG4VP\ioniz <ionizn@gmail.com>
2021-03-21 10:47:19 +0900

    Add Upgrade Select

--- Assets/Scenes/Scripts/ChooseTrain.cs
+++ Assets/Scenes/Scripts/ChooseTrain.cs
@@ -10,10 +10,17 @@ public class ChooseTrain : MonoBehaviour
     public Image case2;
 
     GameObject player;
+    public Sprite normalButtonImage;
+    public Sprite shotgunButtonImage;
+    public Sprite laserButtonImage;
+
+    int sel1;
+    int sel2;
 
     private void Start()
     {
         player = GameObject.Find("Player");
+   
     }
 
     AudioSource audioSource;
@@ -24,21 +31,64 @@ public class ChooseTrain : MonoBehaviour
     }
     public void AttiveUpgrade()
     {
+
+        //Active 되기 전에 무기 선택
+
+        int sel1 = Random.Range(1, 10);
+        int sel2 = Random.Range(1, 10);
+
+
+        if (1 <= sel1 && sel1 <= 4)
+        {
+            case1.sprite = normalButtonImage;
+
+        }
+        else if (5 <= sel1 && sel1 <= 8)
+        {
+
+            case1.sprite = shotgunButtonImage;
+
+        }
+        else
+        {
+            case1.sprite = laserButtonImage;
+
+        }
+
+        if (1 <= sel2 && sel2 <= 4)
+        {
+
+            case2.sprite = normalButtonImage;
+
+        }
+        else if (5 <= sel2 && sel2 <= 8)
+        {
+
+            case2.sprite = shotgunButtonImage;
+
+        }
+        else
+        {
+
+            case2.sprite = laserButtonImage;
+
+        }
+
+
         UpGrade.SetActive(true);
     }
 
     public void OnClickCase1()
     {
-        int ran = Random.Range(1, 10);
-       
 
-        if(1 <= ran  && ran <= 4)
+      
+        if(1 <= sel1  && sel1 <= 4)
         {
             player.GetComponent<Player>().AddTrain(Weapon.WeaponType.NORMAL);
             case1.sprite = Resources.Load<Sprite>("Sprites/Normal");
             Weapon.weapon_type_Int = 0;
         }
-        else if (5 <= ran && ran <= 8)
+        else if (5 <= sel1 && sel1 <= 8)
         {
             player.GetComponent<Player>().AddTrain(Weapon.WeaponType.SHOTGUN);
             case1.sprite = Resources.Load<Sprite>("Sprites/Shotgun");
@@ -61,15 +111,15 @@ public class ChooseTrain : MonoBehaviour
 
     public void OnClickCase2()
     {
-        int ran = Random.Range(1, 10);
 
-        if (1 <= ran && ran <= 4)
+
+        if (1 <= sel2 && sel2 <= 4)
         {
             player.GetComponent<Player>().AddTrain(Weapon.WeaponType.NORMAL);
             case2.sprite = Resources.Load<Sprite>("Sprites/Normal");
             Weapon.weapon_type_Int = 0;
         }
-        else if (5 <= ran && ran <= 8)
+        else if (5 <= sel2 && sel2 <= 8)
         {
             player.GetComponent<Player>().AddTrain(Weapon.WeaponType.SHOTGUN);
             case2.sprite = Resources.Load<Sprite>("Sprites/Shotgun");
